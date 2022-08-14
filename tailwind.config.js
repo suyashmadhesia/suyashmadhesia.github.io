98921ad260c020072fd01335ea79c792d7d0b9a5
Suyash <suyashmadhesia@gmail.com>
2022-08-15 01:24:06 +0530

    work experience and projects

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,9 @@ module.exports = {
   content: ['./dist/*.html', './src/*.html', './src/assets/*.html'],
   theme: {
     screens:{
-      'sm': '600px',
-      // => @media (min-width: 640px) { ... }
-
-      'md': '700px',
-      // => @media (min-width: 768px) { ... }
-
+      'sm': '500px',
+      'md': '600px',
       'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
     },
     extend: {},
   },
